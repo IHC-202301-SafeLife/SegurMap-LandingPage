f4106eacb38ea36d6714d749df7ceb5addbb70c9
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-06-09 15:04:09 -0500

    causas: agregue el navbar~

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customWhite: '#FFFFFF',
+        customOrange: '#D06713',
+        customGray: '#1A1A1A',
+        customPurple: '#0C050C'
+      },
+      container: {
+        center:true,
+      },
+      fontSize: {
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+        '48': '48px',
+        '56': '56px',
+      }
+    },
   },
   plugins: [],
 }
