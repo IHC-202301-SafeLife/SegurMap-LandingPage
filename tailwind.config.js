bcf412d44ebacffabe476e2a97429a942eadf58c
Rimbow002 <enzotrujilloacosta@gmail.com>
2023-06-09 23:46:52 -0500

    feat:add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
         customWhite: '#FFFFFF',
         customOrange: '#D06713',
         customGray: '#1A1A1A',
-        customPurple: '#0C050C'
+        customPurple: '#070307'
       },
       container: {
         center:true,
       },
       fontSize: {
+        '14': '0.5rem',
         '16': '16px',
         '24': '24px',
         '32': '32px',
