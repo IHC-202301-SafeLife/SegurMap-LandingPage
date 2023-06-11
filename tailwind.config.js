a8f6bc64312d0c0c262592f43e87e2b09462467a
RamiroVT <u202123714@upc.edu.pe>
2023-06-10 21:37:14 -0500

    fix: fix elements in hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,25 @@ module.exports = {
     extend: {
       boxShadow: {
         'custom': '0 0 10px #D06713',
-    }
-  },
+      },
+      colors: {
+        customWhite: '#FFFFFF',
+        customOrange: '#D06713',
+        customGray: '#1A1A1A',
+        customPurple: '#070307'
+      },
+      container: {
+        center:true,
+      },
+      fontSize: {
+        '14': '0.5rem',
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+        '48': '48px',
+        '56': '56px',
+      }
+ },
   plugins: [],
 },
 }
